f6c5e90e9cfc4fbe41abcc32bd2a95f963663a07
Adrien Brault <adrien.brault@gmail.com>
2014-01-31 17:31:10 -0800

    fix syntax error

--- grafana/recipes/configure.rb
+++ grafana/recipes/configure.rb
@@ -4,7 +4,7 @@ node[:deploy].each do |application, deploy|
     sed -i -E 's/(graphiteUrl:\s*).+,/\1 "#{deploy['grafana']['graphite_url']}",/g' #{deploy[:deploy_to]}/src/config.js
   eos
 
-  elasticsearch_ip = node[:opsworks][:layers]['elasticsearch'][:instances].first.[:private_ip]
+  elasticsearch_ip = node[:opsworks][:layers]['elasticsearch'][:instances].first[:private_ip]
   command <<-eos
     sed -i -E 's/(elasticsearch:\s*).+,/\1 "http://#{elasticsearch_ip}:9200",/g' #{deploy[:deploy_to]}/src/config.js
   eos
