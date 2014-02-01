c7f573fe5fdc286a948aa316dea8883d900ab191
Adrien Brault <adrien.brault@gmail.com>
2014-02-01 15:58:37 -0800

    Final regex fix ?

--- grafana/recipes/configure.rb
+++ grafana/recipes/configure.rb
@@ -3,12 +3,12 @@ node[:deploy].each do |application, deploy|
   config_path = "#{deploy[:deploy_to]}/current/src/config.js"
 
   update_config_graphite_url = <<-eos
-    sed -i -E 's^(graphiteUrl:\s*).+,^\1 "#{node[:grafana][:graphite_url]}",^g' #{config_path}
+    sed -i -E 's^(graphiteUrl:\s*).+,^graphiteUrl: "#{node[:grafana][:graphite_url]}",^g' #{config_path}
   eos
 
   elasticsearch_ip = node[:opsworks][:layers]['elasticsearch'][:instances].values.first[:private_ip]
   update_config_elasticsearch = <<-eos
-    sed -i -E 's^(elasticsearch:\s*).+,^\1 "http://#{elasticsearch_ip}:9200",^g' #{config_path}
+    sed -i -E 's^(elasticsearch:\s*).+,^elasticsearch: "http://#{elasticsearch_ip}:9200",^g' #{config_path}
   eos
 
   execute update_config_graphite_url
